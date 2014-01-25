88fed4e346929cada3d765186754c5d9503b6b89
OsagieDev <osagieo@gmail.com>
2014-01-24 20:03:29 -0600

    implementing html experiments in jscript

--- js_dom.js
+++ js_dom.js
@@ -6,19 +6,36 @@ document.getElementsByTagName('body')[0].style.backgroundColor = "red"
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
-document.getElementsByTagName('body').style.backgroundColor = "red"
+//document.getElementsByTagName('ul')[1] list.style.backgroundColor = "red"
 document.querySelector('#mybody').style.backgroundColor = "red"
+document.getElementsByTagName("li")[0].innerHTML="Milk";
  
 //RELEASE 2: 
   // Add code here to manipulate the DOM.
+document.getElementById("larger").style.fontSize = "40px"
+document.getElementById('mytable').style.borderColor = "black"
+document.getElementById("mytable").style.borderStyle="solid"
+
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here and replace the console.log code with code that changes the color of some DOM element
- 
+
+document.getElementById("color-switcher").addEventListener('click', function(){
+  console.log('Color Switched!!')
+  document.getElementById("mytable").style.backgroundColor="white"
+}) 
+
 //RELEASE 4 : Experiment on your own
  
- 
- 
+document.getElementById("unshower").addEventListener('click', function(){
+  console.log('Hidden Stuff')
+  document.getElementById('hidenseek').style.visibility='hidden'
+})
+
+document.getElementById("shower").addEventListener('click', function(){
+  console.log('Showing Stuff')
+  document.getElementById('hidenseek').style.visibility='visible'
+})
  
  
  
